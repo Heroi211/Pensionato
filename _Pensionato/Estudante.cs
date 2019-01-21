cb101ca07d406978efd22bfee2e9ecc2936d91c8
Gabriel Drumond <gabriel_s4@hotmail.com>
2019-01-21 19:06:23 -0200

    Completo!

---
+++ _Pensionato/Estudante.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _Pensionato
+{
+    class Estudante
+    {
+        public string nome { get; set; }
+        public string email { get; set; }
+        public int quarto { get; set; }
+
+        public Estudante(string nome, string email, int quarto)
+        {
+            this.nome = nome;
+            this.email = email;
+            this.quarto = quarto;
+        }
+
+        public override string ToString()
+        {
+            return quarto + ": " +
+                nome + ", " + email;
+        }
+
+
+    }
+}
---
+++ _Pensionato/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _Pensionato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Estudante[] aluguel = new Estudante[10];
+
+            for(int i =0;i<n;i++)
+            {
+                Console.WriteLine("Aluguel #" + (i + 1));
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                aluguel[quarto] = new Estudante(nome, email, quarto);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Quartos Ocupados: ");
+
+            for(int i =0;i<10;i++)
+            {
+                if(aluguel[i] != null)
+                {
+                    Console.WriteLine(aluguel[i]);
+                }
+            }
+            Console.WriteLine();
+
+
+            
+        }
+    }
+}
